d54a84d1564f84d5bbc87f280ec1976534c15418
Michael Henry <drmikehenry@drmikehenry.com>
2011-10-13 10:20:01 -0400

    Added primitive UltiSnips support for D.

---
+++ UltiSnips/d.snippets.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# vim:set fileencoding=utf8:
+
+import sys
+import re
+
+from sniputil import put
+
+from sniputil import snip, bsnip, wsnip
+from sniputil import abbr, babbr, wabbr
+
+
+# Snippets are now cleared in "clearsnippets" directory.
+put("extends c\n")
+
