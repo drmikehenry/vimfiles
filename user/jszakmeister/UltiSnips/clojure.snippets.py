a18a1dd099e7c3497bbaf057c986937e5c94a1c3
John Szakmeister <john@szakmeister.net>
2013-09-02 11:09:24 -0400

    user/jszakmeister: update clojure snippets
    These are converted from David Nolen's clojure-snippets repo for
    Yasnippets:
        https://github.com/swannodette/clojure-snippets

--- user/jszakmeister/UltiSnips/clojure.snippets.py
+++ user/jszakmeister/UltiSnips/clojure.snippets.py
@@ -27,13 +27,139 @@ from sniputil import snip, bsnip, wsnip
 from sniputil import abbr, babbr, wabbr
 
 
-bsnip("ns", "(ns ...)", r"""
-(ns ${1:package.name}
-  ${2:(:require ${3:[some.package :as pkg]
-                (some.package [subpkg1 :as p1]
-                              [subpkg2 :as p2])})}${4:
-  (:use ${5:[some.package :only [foo bar]]
-            foo.bar})})
+# bsnip("ns", "(ns ...)", r"""
+# (ns ${1:package.name}
+#   ${2:(:require ${3:[some.package :as pkg]
+#                 (some.package [subpkg1 :as p1]
+#                               [subpkg2 :as p2])})}${4:
+#   (:use ${5:[some.package :only [foo bar]]
+#             foo.bar})})
 
+# $0
+# """)
+
+bsnip("c", "comment", r"""
+(comment
+  $0
+  )
+""")
+
+wsnip("condp", "condp", r"""
+(condp ${1:pred} ${2:expr}
+  $0)
+""")
+
+bsnip("defm", "defmethod", r"""
+(defmethod ${1:name} ${2:match}
+  [${3:args}]
+  $0)
+""")
+
+bsnip("defmm", "defmulti", r"""
+(defmulti ${1:name} ${2:dispatch-fn})
 $0
+""")
+
+bsnip("defn", "defn", r"""
+(defn ${1:name} ${2:
+  "${3:doc-string}"
+  }[${4:arg-list}]
+  $0)
+""")
+
+bsnip("defp", "defprotocol", r"""
+(defprotocol ${1:Name}
+  $0)
+""")
+
+bsnip("defr", "defrecord", r"""
+(defrecord ${1:Name} [${2:fields}]
+  ${3:Protocol}
+  $0)
+""")
+
+bsnip("deft", "deftype", r"""
+(deftype ${1:Name} [${2:fields}]
+  ${3:Protocol}
+  $0)
+""")
+
+wsnip("f", "fresh", r"""
+(fresh [${1:vars}]
+  $0)
+""")
+
+wsnip("fn", "fn", r"""
+(fn [${1:arg-list}] $0)
+""")
+
+wsnip("if", "if", r"""
+(if ${1:test-expr}
+  ${2:then-expr}
+  ${3:else-expr})
+$0
+""")
+
+bsnip("import", "import", r"""
+(:import [${1:package}])
+$0
+""")
+
+wsnip("kwargs", "keyword args", r"""
+& {:keys [${1:keys}] :or {${2:defaults}}}
+$0
+""")
+
+wsnip("let", "let", r"""
+(let [$0])
+""")
+
+wsnip("letfn", "letfn", r"""
+(letfn [(${1:name) [${2:args}]
+          $0)])
+""")
+
+wsnip("m", "method", r"""
+(${1:name} [${2:this} ${3:args}]
+  $0)
+""")
+
+# TODO:compute the expected namespace.
+bsnip("ns", "ns", r"""
+(ns ${1:namespace}
+  $0)
+""")
+
+bsnip("perf", "perf", r"""
+(dotimes [_ 10]
+  (time
+    (dotimes [_ ${1:times}]
+      $0)))
+""")
+
+bsnip("pm", "protocol method", r"""
+(${1:name} [${2:this} ${3:args}])
+$0
+""")
+
+wsnip("refer", "refer", r"""
+(:refer-clojure :exclude [$0])
+""")
+
+wsnip("require", "require", r"""
+(:require [${1:namespace} :as [$0]])
+""")
+
+wsnip("rn", "run n", r"""
+(run ${1:n} [q]
+  $0)
+""")
+
+wsnip("rstar", "run*", r"""
+(run* [q]
+  $0)
+""")
+
+wsnip("use", "use", r"""
+(:use [${1:namespace} :only [$0]])
 """)
