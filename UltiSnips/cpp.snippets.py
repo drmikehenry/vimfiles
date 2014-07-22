c2e32307e87ba4d667890726c54ad929900ab79f
John Szakmeister <john@szakmeister.net>
2014-07-22 08:32:25 -0400

    Let cpp filetypes use snippets from c.

---
+++ UltiSnips/cpp.snippets.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# vim:set fileencoding=utf8:
+
+import os
+import sys
+import re
+import setpypath
+
+from sniputil import put
+
+from sniputil import snip, bsnip, wsnip
+from sniputil import abbr, babbr, wabbr
+
+
+put(r"""
+priority -5
+
+extends c
+""")
