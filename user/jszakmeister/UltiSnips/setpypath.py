9396d84f8ed89611f6beb113b8178e8355c30e98
John Szakmeister <john@szakmeister.net>
2014-08-03 10:29:31 -0400

    user/jszakmeister: remove Szakmeister's config
    It has been split into its own repository, but still builds off the main
    configuration using the vimuser functionality.

--- user/jszakmeister/UltiSnips/setpypath.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# vim:set fileencoding=utf8: #
-
-import sys
-import os
-
-# Augment sys.path with top-level pylib.
-thisDir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.abspath(thisDir + "/../../../pylib"))
--- user/jszakmeister/machine/yoda.local.vim
+++
@@ -1,1 +0,0 @@
-let g:FontSize = 15
