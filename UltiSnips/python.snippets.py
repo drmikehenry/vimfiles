cf801f9356bc85f6750e69b6824e6be00d8fafc6
John Szakmeister <john@szakmeister.net>
2014-11-19 03:30:17 -0500

    Add a .snippets.py template for use with the skel.

--- UltiSnips/python.snippets.py
+++ UltiSnips/python.snippets.py
@@ -173,6 +173,24 @@ bsnip("pdb", "pdb.set_trace()", r"""
 import pdb; pdb.set_trace()
 """)
 
+# Template for a new .snippets.py file.
+bsnip("template_python.snippets.py", "new snippet template", r"""
+#!/usr/bin/env python
+# vim:set fileencoding=utf8:
+
+import os
+import sys
+import re
+import setpypath
+
+from sniputil import put
+
+from sniputil import snip, bsnip, wsnip
+from sniputil import abbr, babbr, wabbr
+
+$0
+""")
+
 # @todo Snippets for print, logging, formatting strings "%d,%d" % (a,b).
 '''
 
