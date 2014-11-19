a2d3de3dbba4e4e863aa2a580125f18c8c275d46
John Szakmeister <john@szakmeister.net>
2014-11-19 04:18:54 -0500

    Add a template for .syntastic_c_config files.
    This makes it a little easier to add include paths relative to the
    .syntastic_c_config file.

---
+++ UltiSnips/vim.snippets.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# vim:set fileencoding=utf8:
+
+import os
+import sys
+import re
+import setpypath
+
+from sniputil import put
+
+from sniputil import snip, bsnip, wsnip
+from sniputil import abbr, babbr, wabbr
+
+
+bsnip("template_vim..syntastic_c_config", "syntastic c config template", r"""
+let s:include_dirs = [$0]
+let g:syntastic_c_include_dirs =
+            \ map(s:include_dirs, 'expand("<sfile>:p:h") . "/" . v:val'
+""")
