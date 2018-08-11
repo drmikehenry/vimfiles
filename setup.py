c06bbd1898232922ea4399d23804c873f063dc6a
Michael Henry <drmikehenry@drmikehenry.com>
2018-08-11 11:54:32 -0400

    Tidy up setup.py formatting and verify it works with Python 2/3.

--- setup.py
+++ setup.py
@@ -1,52 +1,61 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import os
 import re
-import sys
-from subprocess import Popen, PIPE, STDOUT
+import subprocess
+
 
 class RunError(Exception):
     pass
 
-def runArgsOutErr(args):
-    p = Popen(args, stdout=PIPE, stderr=PIPE)
+
+def run_args_out_err(args):
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     return out, err
 
-def runArgs(args):
-    out, err = runArgsOutErr(args)
+
+def run_args(args):
+    out, err = run_args_out_err(args)
     if err:
-        raise RunError("%s" % str(args))
+        raise RunError('%s' % str(args))
     return out
 
-def joinLines(lines):
-    return "".join([line + "\n" for line in lines])
 
-def readLines(path):
-    f = open(path, "r")
+def join_lines(lines):
+    return ''.join([line + '\n' for line in lines])
+
+
+def read_lines(path):
+    f = open(path, 'r')
     lines = f.read().splitlines()
     f.close()
     return lines
 
-def writeLines(path, lines):
-    f = open(path, "w")
-    f.write(joinLines(lines))
+
+def write_lines(path, lines):
+    f = open(path, 'w')
+    f.write(join_lines(lines))
     f.close()
 
+
 def main():
-    homePath = os.path.expanduser("~")
-    vimrcPath = os.path.join(homePath, ".vimrc")
-    if os.path.isfile(vimrcPath):
-        vimrcLines = readLines(vimrcPath)
+    home_path = os.path.expanduser('~')
+    vimrc_path = os.path.join(home_path, '.vimrc')
+    if os.path.isfile(vimrc_path):
+        vimrc_lines = read_lines(vimrc_path)
     else:
-        vimrcLines = []
-    runtimeLine = "runtime vimrc"
-    for line in vimrcLines:
-        if re.match(r'(\s*"\s*)?' + re.escape(runtimeLine) + r"\s*$", line):
+        vimrc_lines = []
+    runtime_line = 'runtime vimrc'
+    for line in vimrc_lines:
+        if re.match(r'(\s*"\s*)?' + re.escape(runtime_line) + r'\s*$', line):
             break
     else:
-        vimrcLines.insert(0, runtimeLine)
-        writeLines(vimrcPath, vimrcLines)
+        vimrc_lines.insert(0, runtime_line)
+        write_lines(vimrc_path, vimrc_lines)
+
 
 if __name__ == '__main__':
     main()
