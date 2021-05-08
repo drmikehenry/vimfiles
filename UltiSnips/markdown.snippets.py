48a376ee66d4704ab79d1f7d7106f085ef57fdae
Michael Henry <drmikehenry@drmikehenry.com>
2021-05-08 15:06:45 -0400

    Add a few snippets for Markdown.

---
+++ UltiSnips/markdown.snippets.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# vim:set fileencoding=utf8:
+
+import sys
+import re
+import setpypath
+
+from sniputil import snip, bsnip, wsnip
+from sniputil import abbr, babbr, wabbr
+from sniputil import put
+
+put(r"""
+priority -5
+
+global !p
+from sniputil import betterVisual
+endglobal
+""")
+
+bsnip("code", "code block", r"""
+\`\`\`${1:python}
+$0
+\`\`\`
+""")
+
+bsnip("sh", "sh code block", r"""
+\`\`\`sh
+$0
+\`\`\`
+""")
+
+bsnip("python", "python code block", r"""
+\`\`\`python
+$0
+\`\`\`
+""", aliases=['py'])
+
+bsnip("ruby", "ruby code block", r"""
+\`\`\`ruby
+$0
+\`\`\`
+""")
+
+bsnip("rust", "rust code block", r"""
+\`\`\`rust
+$0
+\`\`\`
+""")
+
+bsnip("text", "text code block", r"""
+\`\`\`
+$0
+\`\`\`
+""")
+
+wsnip("date", "today's date", r"""`!v strftime("%Y-%m-%d")`""")
