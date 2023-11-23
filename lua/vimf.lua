5f08d6686a7717c35251c9118b57330bd313e3be
Michael Henry <drmikehenry@drmikehenry.com>
2023-11-23 14:19:51 -0500

    Move per-plugin configuration into `lua/vimf/plugin/`.

--- lua/vimf.lua
+++ lua/vimf.lua
@@ -6,10 +6,10 @@ local M = {}
 
 M.setup = function()
     vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-    require('vimf.cmp').setup()
-    require('vimf.lsp').setup()
-    require('vimf.telescope').setup()
-    require('vimf.which-key').setup()
+    require('vimf.plugin.cmp').setup()
+    require('vimf.plugin.lsp').setup()
+    require('vimf.plugin.telescope').setup()
+    require('vimf.plugin.which-key').setup()
 end
 
 return M
