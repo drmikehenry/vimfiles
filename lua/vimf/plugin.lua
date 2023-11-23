5c4fd84a864d0bc39322c7c086a258d3c2478ac7
Michael Henry <drmikehenry@drmikehenry.com>
2023-11-23 14:49:03 -0500

    Fix LUA `vimf.plugin.enabled()`.
    Lua considers only `false` and `nil` to be false, whereas the underlying
    Vim function uses `0` for false.

--- lua/vimf/plugin.lua
+++ lua/vimf/plugin.lua
@@ -1,15 +1,15 @@
 local M = {}
 
 M.enabled = function(plugin_name)
-    return vim.fn["vimf#plugin#enabled"](plugin_name)
+    return vim.fn["vimf#plugin#enabled"](plugin_name) ~= 0
 end
 
 M.enable = function(plugin_name)
-    return vim.fn["vimf#plugin#enable"](plugin_name)
+    vim.fn["vimf#plugin#enable"](plugin_name)
 end
 
 M.disable = function(plugin_name)
-    return vim.fn["vimf#plugin#disable"](plugin_name)
+    vim.fn["vimf#plugin#disable"](plugin_name)
 end
 
 return M
